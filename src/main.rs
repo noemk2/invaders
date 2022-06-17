59e761fb5977dc625e73f68767aec8e011a523fb
Noe Huaccharaque <noemk2@gmail.com>
2022-06-16 20:16:37 -0500

    learn

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
-use std::io::{stdout, Write};
 use crossterm::{
-    ExecutableCommand, QueueableCommand,
-    terminal, cursor, style::{self, Stylize}, Result
+    cursor,
+    style::{self, Stylize},
+    terminal, ExecutableCommand, QueueableCommand, Result,
 };
+use std::io::{stdout, Write};
 
 fn main() -> Result<()> {
-  let mut stdout = stdout();
+    let mut stdout = stdout();
 
-  stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
 
-  for y in 0..40 {
-    for x in 0..150 {
-      if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
-        // in this loop we are more efficient by not flushing the buffer.
-        stdout
-          .queue(cursor::MoveTo(x,y))?
-          .queue(style::PrintStyledContent( "█".magenta()))?;
-      }
+    for y in 0..40 {
+        for x in 0..150 {
+            if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
+                // in this loop we are more efficient by not flushing the buffer.
+                stdout
+                    .queue(cursor::MoveTo(x, y))?
+                    .queue(style::PrintStyledContent("█".magenta()))?;
+            }
+        }
     }
-  }
-  stdout.flush()?;
-  Ok(())
+    stdout.flush()?;
+    Ok(())
 }
