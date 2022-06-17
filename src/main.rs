9aabd700f97aaecee138d11ba39645a5b8796afa
Noe Huaccharaque <noemk2@gmail.com>
2022-06-16 19:46:05 -0500

    initial_view

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, Write};
+use crossterm::{
+    ExecutableCommand, QueueableCommand,
+    terminal, cursor, style::{self, Stylize}, Result
+};
+
+fn main() -> Result<()> {
+  let mut stdout = stdout();
+
+  stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+  for y in 0..40 {
+    for x in 0..150 {
+      if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
+        // in this loop we are more efficient by not flushing the buffer.
+        stdout
+          .queue(cursor::MoveTo(x,y))?
+          .queue(style::PrintStyledContent( "█".magenta()))?;
+      }
+    }
+  }
+  stdout.flush()?;
+  Ok(())
 }
